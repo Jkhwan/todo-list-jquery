cc2d543c0691495e0046be5d8b4164fb00677884
Jason Wan <jwan823@gmail.com>
2013-11-06 07:28:40 +0000

    Added enhancements

--- js/app.js
+++ js/app.js
@@ -6,8 +6,8 @@ $(function() {
 			$('div.alert-danger').text("Text field is required.").show();
 		} else {
 			$('div.alert-danger').text("").hide();
-			var newListItem = $("<li></li>").text($('input.add').val());
-			newListItem.addClass("list-group-item").append($('<i class="fa fa-fw fa-square-o"></i>')).append($('<i class="fa fa-fw fa-eraser"></i>'));
+			var newListItem = $("<li></li>").append($('<span class="itemName"></span>').text($('input.add').val()));
+			newListItem.addClass("list-group-item").append($('<i class="fa fa-fw fa-square-o"></i>')).append($('<i class="fa fa-fw fa-edit"></i>')).append($('<input type="text" class="itemTextbox">')).append($('<i class="fa fa-fw fa-eraser"></i>'));
 			$("ul#todo").append(newListItem.fadeIn());
 			$('input.add').val("");
 		}
@@ -22,8 +22,26 @@ $(function() {
 		$(this).parent().remove();
 	});
 
-	$('ul#todo').on("mouseover", "i.fa-square-o", function() {
+	$('ul#todo').on("mouseover", "i", function() {
 		$(this).addClass("mouseover");
+	});
+
+	$('ul#todo').on("mouseout", "i", function() {
+		$(this).removeClass("mouseover");
+	});
+
+	$('ul#todo').on("click", "i.fa-edit", function() {
+		$(this).toggleClass("edit");
+		$(this).parent().find('input.itemTextbox').toggleClass("edit");
+		var txtbox = $(this).parent().find('input.itemTextbox');
+		var name = $(this).parent().find('.itemName');
+		if(txtbox.hasClass('edit')) {
+			name.hide();
+			txtbox.val(name.text());
+		} else {
+			name.text(txtbox.val());
+			name.show();
+		}
 	});
 
 	$('ul#todo').on("click", ".fa-square-o", function() {
